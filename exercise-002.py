f6e769f8ab9b8ffa34452be4f718ddd45d96dd75
Phil <279342427+fsdevphil@users.noreply.github.com>
2026-07-09 22:51:45 +0100

    Create exercise-002.py

---
+++ exercise-002.py
@@ -0,0 +1,7 @@
+sword_damage = 10
+start_health = 100
+end_health = start_health - sword_damage
+
+print(f"Sam's health is: {start_health}")
+print(f"Sam takes {sword_damage} damage...")
+print(f"Sam's health is: {end_health}")
