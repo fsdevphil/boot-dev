c9d279a21608c75bf626ca84dcdc1aaa0e409ae8
Phil <279342427+fsdevphil@users.noreply.github.com>
2026-07-12 14:52:09 +0100

    Create ex-013.py

---
+++ ex-013.py
@@ -0,0 +1,11 @@
+# Syntax error : mismatched quotes
+print("Starting up game server...')
+
+# Fixed
+print("Starting up game server...")
+
+# Syntax error : spelling error
+prnt("local game server is listening on port 8080")
+
+# Fixed
+print("local game server is listening on port 8080")
