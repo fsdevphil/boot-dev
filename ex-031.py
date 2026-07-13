5f291d003a8e8d341479bfcd014469b39a121eb5
Phil <279342427+fsdevphil@users.noreply.github.com>
2026-07-13 19:54:37 +0100

    Create ex-031.py

---
+++ ex-031.py
@@ -0,0 +1,15 @@
+# Strings
+name_with_single_quotes = 'boot.dev' # not so good
+name_with_double_quotes = "boot.dev" # so good
+
+# Integers
+x = 5 # positive integer
+x = -5 # negative integer
+
+# Floats
+x = 5.2
+x = - 5.2
+
+# Booleans
+isTall = True
+isShort = False
