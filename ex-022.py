372bb75e9e794ef0e54492f0ab95a0bc66e319a2
Phil <279342427+fsdevphil@users.noreply.github.com>
2026-07-13 18:27:26 +0100

    Create ex-022.py

---
+++ ex-022.py
@@ -0,0 +1,17 @@
+player_health = 1000
+
+player_health = 900
+
+print(player_health)
+
+player_health = 800
+
+print(player_health)
+
+player_health = 700
+
+print(player_health)
+
+player_health = 600
+
+print(player_health)
