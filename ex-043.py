1c1b0c26bf5f3d3505e30dcd6e74fdd9ac80812a
Phil <279342427+fsdevphil@users.noreply.github.com>
2026-07-14 13:40:43 +0100

    Create ex-043.py

---
+++ ex-043.py
@@ -0,0 +1,10 @@
+player_name = "Charles"
+greeting = "Well met,"
+quest_start = "The local mine has been taken over by orcs!"
+quest_end = "We need your help taking it back."
+space = " "
+
+complete_greeting = greeting + space + player_name
+print(complete_greeting)
+print(quest_start)
+print(quest_end)
