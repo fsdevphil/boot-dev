730744e31c8ca581702c09a4c5a9ef43a4871c3a
Phil <279342427+fsdevphil@users.noreply.github.com>
2026-07-14 13:52:11 +0100

    Create ex-044.py

---
+++ ex-044.py
@@ -0,0 +1,7 @@
+game_one_score = 97
+game_two_score = 92
+game_three_score = 106
+game_four_score = 105
+
+average_score = (game_one_score + game_two_score + game_three_score + game_four_score) / 4
+print(round(average_score))
